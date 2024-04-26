b8a52f48b98059bb2785cce53854b437831a10c8
FemiFX <162863105+FemiFX@users.noreply.github.com>
2024-04-26 12:39:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,10 +27,10 @@ ABOUT = load_about()
 setup(
     name="tutor-indigo",
     version=ABOUT["__version__"],
-    url="https://github.com/overhangio/tutor-indigo",
+    url="https://github.com/FemiFX/tutor-indigo",
     project_urls={
         "Documentation": "https://docs.tutor.edly.io/",
-        "Code": "https://github.com/overhangio/tutor-indigo",
+        "Code": "https://github.com/FemiFX/tutor-indigo",
         "Issue tracker": "https://github.com/overhangio/tutor-indigo/issues",
         "Community": "https://discuss.openedx.org",
     },
